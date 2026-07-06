7dd4965135849bcd023bd46fe3f0c65c5886a3fb
OmSikawat <omsikawat67@gmail.com>
2026-07-06 23:59:58 +0530

    Structure created

---
+++ src/main/java/com/deploybrain/DeploybrainApplication.java
@@ -0,0 +1,13 @@
+package com.deploybrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploybrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploybrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploybrain/DeploybrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploybrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploybrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
